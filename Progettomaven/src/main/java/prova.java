0c6b713ed8a880e4f7c43fb530d12790489c470b
simone.viotti <simone.viotti@host.docker.internal>
2023-12-18 11:32:33 +0100

    dio

--- Progettomaven/src/main/java/prova.java
+++ Progettomaven/src/main/java/prova.java
@@ -1,8 +1,10 @@
 
+
 public class prova {
 
 	public static void main(String[] args) {
 		System.out.println("ciao mondo");
+		System.out.println("ciao mondo di merda");
 		System.out.println("ciao mondo di merda");
 	}
 
